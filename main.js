502ca292a675069890886d0e214ef61deb422ed1
ShreyasJ1 <109956165+ShreyasJ1@users.noreply.github.com>
2023-09-16 15:02:30 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,61 @@
+img = "";
 status = "";
-
+objects = [];
+video = "";
+object_to_search = "";
 function setup() {
-    canvas = createCanvas(600,500);
+    canvas = createCanvas(480, 380);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(600, 500);
+    video.size(380, 380);
     video.hide();
+
 }
 
-function start()
-{
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status - Detecting Objects";
-    object = document.getElementById("text_box").value;
-    console.log(object);
+
+function draw() {
+    image(video, 0, 0, 480, 380);
+    if (status != "") {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status - Object Mentioned Not Found";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are " + objects.length;
+            fill("black");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            if (objects[i].label == object_to_search) {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("status").innerHTML = "Status - Object Mentioned Found";
+                i = 0;
+            }
+        }
+    }
 }
 
 function modelLoaded() {
-    console.log("Model Loaded");
+    console.log("CocoSSD is initialized");
     status = true;
-
 }
 
-function draw() {
-    image(video, 0, 0, 600, 500);
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        objects = results;
+
+    }
+}
+
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status - Detecting Objects";
+    object_to_search = document.getElementById("text_box").value;
+    console.log(object_to_search);
 }
